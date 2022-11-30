d8a9220c4573a0b075da3d90dc8f923b1aca9124
Leo Roettele <leo.roettele@liip.ch>
2022-11-30 16:50:45 +0100

    create find endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
+import { RedirectItem } from './types';
 
-export default defineEndpoint((router) => {
-	router.get('/', (_req, res) => res.send('Hello, World!'));
+export default defineEndpoint((router, { services }) => {
+	const { ItemsService } = services;
+
+	router.get('/find', async (req, res) => {
+
+		const redirectsService = new ItemsService(
+			'redirects',
+			{ schema: req.schema, accountability: req.accountability }
+		);
+
+		const redirects: RedirectItem[] =
+			await redirectsService.readByQuery({ fields: ['*'] });
+
+		const response: RedirectItem[] =
+			redirects.filter((redirectItem) => {
+				const regex: RegExp = new RegExp(redirectItem.from);
+				return redirectItem.regex && regex.test(req.query.path);
+			});
+
+		res.send(response[0]?.to);
+	});
 });
