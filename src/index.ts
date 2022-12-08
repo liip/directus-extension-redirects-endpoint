a491fa1599ee99f98f71216f1fafa92cf681f4ce
Leo Roettele <leo.roettele@liip.ch>
2022-12-08 15:05:28 +0100

    first search string on db and if no match, fetch regex redirects paginated and compare them in ts.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 import { RedirectItem } from './types';
 
-export default defineEndpoint((router, { services }) => {
-	const { ItemsService } = services;
+const findMatchingRegexRedirects = async (redirectsService, path: string, page = 0): Promise<RedirectItem | undefined> => {
+  const redirectsWithRegex: RedirectItem[] = await redirectsService.readByQuery({
+    fields: ['*'],
+    filter: { regex: { _eq: true }},
+    page: page,
+  });
 
-	router.get('/find', async (req, res) => {
-
-		const redirectsService = new ItemsService(
-			'redirects',
-			{ schema: req.schema, accountability: req.accountability }
-		);
-
-		const redirects: RedirectItem[] =
-			await redirectsService.readByQuery({ fields: ['*'] });
-
-		const response: RedirectItem[] =
-			redirects.filter((redirectItem) => {
-				const regex: RegExp = new RegExp(redirectItem.from);
-        return redirectItem.regex ? regex.test(req.query.path) : redirectItem.from === req.query.path;
+  if (redirectsWithRegex.length) {
+    const match: RedirectItem | undefined =
+      redirectsWithRegex.find((redirectItem) => {
+        const regex: RegExp = new RegExp(redirectItem.from);
+        return regex.test(path);
       });
 
-		res.send(response[0]?.to);
-	});
+    return match  || (await findMatchingRegexRedirects(redirectsService, path, page+1));
+  }
+  return;
+}
+
+export default defineEndpoint((router, { services }) => {
+  const { ItemsService } = services;
+
+  router.get('/find', async (req, res) => {
+
+    const redirectsService = new ItemsService(
+      'redirects',
+      {
+        schema: req.schema,
+        accountability: req.accountability
+      }
+    );
+
+    const exactMatches = await redirectsService.readByQuery({
+      fields: ['*'],
+      filter: {
+        regex: { _eq: false },
+        from: { _eq: req.query.path },
+      },
+    });
+
+    if (exactMatches.length) {
+      res.send(exactMatches[0]?.to);
+    } else {
+      const regexMatches: RedirectItem | undefined = await findMatchingRegexRedirects(redirectsService, req.query.path);
+      regexMatches ? res.send(regexMatches?.to) : res.send('');
+    }
+  });
 });
